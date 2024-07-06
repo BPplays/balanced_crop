ef08a744138ea626f25762c3ad68ff4df28e3533
BPplays <andymalbp@gmail.com>
2024-07-06 09:26:58 -0700

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/BPplays/balanced_crop
 
 go 1.22.0
 
-require github.com/tidbyt/go-libwebp v0.0.0-20230922075150-fb11063b2a6a // indirect
+require (
+	github.com/pixiv/go-libwebp v0.1.1 // indirect
+	github.com/tidbyt/go-libwebp v0.0.0-20230922075150-fb11063b2a6a // indirect
+	golang.org/x/image v0.18.0 // indirect
+	gopkg.in/gographics/imagick.v3 v3.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"image"
 	"image/color"
+	"image/draw"
 	"log"
+	"os"
 
-	"github.com/tidbyt/go-libwebp/test/util"
-	"github.com/tidbyt/go-libwebp/webp"
+	"golang.org/x/image/webp"
+	"gopkg.in/gographics/imagick.v3/imagick"
 )
 
 
@@ -22,39 +23,91 @@ func crop_brd(img *image.RGBA, border_percent float64) {
 	}
 }
 
+// rgbaToMagickWand converts an image.RGBA to a MagickWand
+func rgbaToMagickWand(img *image.RGBA, mw *imagick.MagickWand) error {
+    bounds := img.Bounds()
+    width := bounds.Dx()
+    height := bounds.Dy()
+
+    // Create a new image in MagickWand
+    if err := mw.NewImage(uint(width), uint(height), imagick.NewPixelWand()); err != nil {
+        return err
+    }
+
+    // Set the image format
+    if err := mw.SetImageFormat("RGBA"); err != nil {
+        return err
+    }
+
+    // Import pixel data
+    pixelData := img.Pix
+    if err := mw.ImportImagePixels(0, 0, uint(width), uint(height), "RGBA", imagick.PIXEL_CHAR, pixelData); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func imageToRGBA(src *image.Image) *image.RGBA {
+
+    // No conversion needed if image is an *image.RGBA.
+    if dst, ok := (*src).(*image.RGBA); ok {
+        return dst
+    }
+
+    // Use the image/draw package to convert to *image.RGBA.
+    b := (*src).Bounds()
+    dst := image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+    draw.Draw(dst, dst.Bounds(), (*src), b.Min, draw.Src)
+    return dst
+}
 
 func read_crop(in string, out string) {
-	var err error
+	var img image.RGBA
 
-	// Read binary data
-	data := util.ReadFile(in)
-
-	// Decode
-	options := &webp.DecoderOptions{UseThreads: true}
-	img, err := webp.DecodeRGBA(data, options)
+	file, err := os.Open("example.webp")
 	if err != nil {
-		panic(err)
+		fmt.Println("Error opening file:", err)
+		return
 	}
+	defer file.Close()
 
-	// Create file and buffered writer
-	io := util.CreateFile(out)
-	w := bufio.NewWriter(io)
-	defer func() {
-		w.Flush()
-		io.Close()
-	}()
-
-	config, err := webp.ConfigLosslessPreset(9)
+	// Decode the WebP file
+	imgw, err := webp.Decode(file)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println("Error decoding WebP file:", err)
+		return
 	}
 
-	// Encode into WebP
-	if err := webp.EncodeRGBA(w, img, config); err != nil {
-		panic(err)
-	}
+	img = *(imageToRGBA(&imgw))
+
+    // Create a new MagickWand
+    mw := imagick.NewMagickWand()
+    defer mw.Destroy()
+
+    if err := rgbaToMagickWand(&img, mw); err != nil {
+        log.Fatalf("Error converting RGBA to MagickWand: %v", err)
+    }
+
+    // Set WebP format
+    mw.SetImageFormat("WEBP")
+
+    // Enable lossless compression
+    mw.SetOption("webp:lossless", "true")
+
+    // Set the compression method to the highest effort (maximum value is 6)
+    mw.SetOption("webp:method", "6")
+
+    // Write the image
+    if err := mw.WriteImage("output.webp"); err != nil {
+        log.Fatalf("Error writing image: %v", err)
+    }
+
+    log.Println("Successfully converted image to lossless WebP with maximum compression effort")
 }
 
 func main() {
+	imagick.Initialize()
+    defer imagick.Terminate()
 	read_crop("test.webp", "out.webp")
 }
